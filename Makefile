ad7aaec25ea06941361f7935583e26e2c5f78050
Eric Moritz <eric@themoritzfamily.com>
2015-02-15 02:57:35 -0500

    added bootstrap and the queue form

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
-all:
+all: build/music-reviews.js build/music-reviews.css
+
+build/music-reviews.js:
 	node_modules/.bin/browserify src/music-reviews.js -o build/music-reviews.js
+
+build/music-reviews.css:
+	./node_modules/.bin/lessc src/style.less build/music-reviews.css
 
 watch:
 	node_modules/.bin/watchify src/music-reviews.js -o build/music-reviews.js
--- src/music-reviews.js
+++ src/music-reviews.js
@@ -2,16 +2,26 @@ require("6to5/polyfill");
 var URI = require("uri-template-lite").URI;
 var React = require("react");
 var $ = require("jquery");
+var bs = require('react-bootstrap');
+var jsonld = require('jsonld');
+
+function ensureArray(x) {
+    if(x.find) {
+	return x;
+    } else {
+	return [x]
+    }
+}
 
 function iriTemplateRender(iriTemplate, variables) {
-    var template = iriTemplate['hydra:template'];
+    var template = iriTemplate['template'];
     var bindings = {}
     for(var prop in variables) {
-	var mapping = iriTemplate['hydra:mapping'].find(
-	    mapping => mapping['hydra:property'] == prop
+	var mapping = ensureArray(iriTemplate['mapping']).find(
+	    mapping => mapping['property'] == prop
 	)
 	if(mapping) {
-	    bindings[mapping['hydra:variable']] = variables[prop]
+	    bindings[mapping['variable']] = variables[prop]
 	}
     }
     return URI.expand(template, bindings)
@@ -22,10 +32,12 @@ var UserMenu = React.createClass({
 	var queueUrl = "#" + this.props.data.queue;
 	var seenUrl = "#" + this.props.data.seen;
 	return (
-	    <ul>
-		<li><a href={queueUrl}>Queue</a></li>
-		<li><a href={seenUrl}>Seen</a></li>
-	    </ul>
+	    <bs.Navbar>
+              <bs.Nav>
+  	        <bs.NavItem entryKey={1} href={queueUrl}>Queue</bs.NavItem>
+		<bs.NavItem entryKey={2} href={seenUrl}>Seen</bs.NavItem>
+              </bs.Nav>
+            </bs.Navbar>
 	)
     }
 })
@@ -34,8 +46,7 @@ var Review = React.createClass({
     render() {
 	return (
 	    <span>
-		<span className="rating">{this.props.data.reviewRating.ratingValue}</span>
-		:
+		<bs.Badge>{this.props.data.reviewRating.ratingValue}</bs.Badge><span> </span>
 		<a href={this.props.data.url}>{this.props.data.about.name}</a>
 		<span> by {this.props.data.about.byArtist.name}</span>
             </span>
@@ -43,20 +54,70 @@ var Review = React.createClass({
     }
 })
 
+var QueueForm = React.createClass({
+    handleSubmit(e) {
+	e.preventDefault();
+	var properties = {}
+	
+	var ratingValue = this.refs.ratingValue.getDOMNode().value.trim();
+	if(ratingValue) {
+	    properties['ratingValue'] = ratingValue;
+	}
+
+	var datePublished = this.refs.datePublished.getDOMNode().value.trim();
+	if(datePublished) {
+	    properties['datePublished'] = datePublished
+	}
+
+	window.location.hash = "#" + iriTemplateRender(
+	    this.props.data,
+	    properties
+	);
+    },
+    render() {
+	return (
+	    <form onSubmit={this.handleSubmit}>
+	    <div>
+	    <label>Pub Date &gt;=</label><input ref="datePublished" placeholder="YYYY-MM-DD" />
+	    </div>
+	    <div>
+	    <label>Score &gt;=</label><input ref="ratingValue" placeholder="number" />
+	    </div>
+	    <input type="submit" value="update" />
+	    </form>
+	)
+    }
+});
+
 var ReviewList = React.createClass({
     render() {
+	var formColContent;
 	var members = this.props.data.member;
+	var listColContent = (() => {
+	    if(members.length) {
+		var items = members.map(
+		    x => <bs.ListGroupItem><Review data={x} /></bs.ListGroupItem>
+		)
+		return <bs.ListGroup>{items}</bs.ListGroup>
+	    } else {
+		return <div>No items</div>
+	    }
+	})();
 
-	if(members.length) {
-	    var items = members.map(
-		x => <li><Review data={x} /></li>
-	    )
-	    return <ul>{items}</ul>
-	} else {
-	    return <div>No items</div>
+	if(this.props.data.queueForm) {
+	    formColContent = <QueueForm data={this.props.data.queueForm} />
 	}
+
+	return (
+	    <bs.Grid>
+		<bs.Row>
+		  <bs.Col md={8}>{listColContent}</bs.Col>
+		  <bs.Col md={4}>{formColContent}</bs.Col>
+		</bs.Row>
+	    </bs.Grid>
+	)
     }
-})
+});
 
 var LoginForm = React.createClass({
     handleSubmit(e) {
@@ -75,16 +136,68 @@ var LoginForm = React.createClass({
     }
 });
 
+var Loading = React.createClass({
+    render() {
+	return <div><span className="icon-spin">Loading...</span></div>
+    }
+})
+
 var MusicReviewApp = React.createClass({
+    CONTEXT: {
+	"@context": {
+	    "hydra": "http://www.w3.org/ns/hydra/core#",
+	    "mrs": "http://rdf.vocab-ld.org/vocabs/music-reviews.jsonld#",
+	    "schema": "http://schema.org/",
+	    "template": "hydra:template",
+	    "mapping": "hydra:mapping",
+	    "property": "hydra:property",
+	    "variable": "hydra:variable",
+	    "member": "hydra:member",
+
+	    "queue": {"@id": "mrs:queue", "@type": "@id"},
+	    "seen": {"@id": "mrs:seen", "@type": "@id"},
+	    "queueForm": "mrs:queueForm",
+	    "loginForm": "mrs:loginForm",
+	    "User": "mrs:User",
+	    "user": "mrs:user",
+	    "ReviewList": "mrs:ReviewList",
+
+	    "reviewRating": "schema:reviewRating",
+	    "ratingValue": "schema:ratingValue",
+	    "url": "schema:url",
+	    "about": "schema:about",
+	    "byArtist": "schema:byArtist",
+	    "name": "schema:name",
+	    "datePublished": "schema:datePublished"
+	}
+    },
     getInitialState() {
 	return {data: {}}
     },
     navigate(url) {
+	this.setState({loading:true, data:this.state.data})
 	$.ajax({
 	    url: url,
 	    dataType: 'json',
-	    success: data => this.setState({data:data}),
-	    error: (xhr, status, err) => console.error(url, status, err.toString()) 
+	    success: data => {
+		console.log(data);
+		jsonld.compact(
+		data,
+		this.CONTEXT,
+		(err, compacted) => {
+		    if(err) {
+			console.error(url, err)
+		    } else {
+			console.log(compacted);
+			this.setState({loading:false, data:compacted})
+		    }
+		}
+		)
+	    },
+	    error: (xhr, status, err) => {
+		this.setState({loading:false, data:{}}),
+		console.error(url, status, err.toString()) 
+	    }
 	})
     },
     serverUrl() {
@@ -104,40 +217,43 @@ var MusicReviewApp = React.createClass({
     },
     render() {
 	function hasType(object, type) {
-	    return object['@type'] && object['@type'].find(x => x == type)
+	    return object['@type'] && ensureArray(object['@type']).find(x => x == type)
 	}
 	var nodes = [];
-	var loginForm, userMenu, userObj, reviewList;
+	var content, loginForm, userMenu, userObj, reviewList, loading;
+
+	if(this.state.loading) {
+	    loading = <Loading />
+	}
 
 	if(hasType(this.state.data, 'User')) {
 	    userObj = this.state.data;
 	} else if(this.state.data.user) {
 	    userObj = this.state.data.user;
 	}
-
+	
 	if(this.state.data.loginForm) {
 	    loginForm = <LoginForm data={this.state.data.loginForm} />
 	}
-
+	
 	if(hasType(this.state.data, 'ReviewList')) {
 	    reviewList = <ReviewList data={this.state.data} />
 	}
-
-        if(userObj) {
+	
+	if(userObj) {
 	    userMenu = <UserMenu data={userObj} />
 	}
 
-	return (<div className="MusicReviewApp">
+	return (<bs.Panel bsStyle="primary">
 		{loginForm}
-
 		{userMenu}
-
+		{loading}
 		{reviewList}
-
 		<div>
   		<a href={this.serverUrl()} target="_blank">{this.serverUrl()}</a>
 		</div>
-		</div>)
+	
+		</bs.Panel>)
     }
 });
 
